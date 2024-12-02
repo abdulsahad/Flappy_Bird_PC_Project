b8356fdbe7766caa3582db94d7aba04fbcfe91c6
Abdul Ahad <159277702+abdulsahad@users.noreply.github.com>
2024-12-02 12:29:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
 from screen import game_screen
+from bird import Bird
+
+BIRD_WIDTH = 25
+BIRD_HEIGHT = 30
+BIRD_POSITION_X = 50
+BIRD_POSITION_Y = 50
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 600
-BG_COLOR = (135, 206, 235)  # Sky blue background color
+BG_COLOR = (250, 250, 250)  
+
+
 # Initialization
 screen = game_screen(WINDOW_WIDTH, WINDOW_HEIGHT)
 clock = pygame.time.Clock()
 
+bird = Bird(BIRD_POSITION_X, BIRD_POSITION_Y, BIRD_WIDTH, BIRD_HEIGHT)
 running = True
 while running:
     screen.fill((BG_COLOR))  # Sky blue background
@@ -15,7 +24,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.display.flip()
+    bird.drawing(screen)
+    pygame.display.update()
     clock.tick(30)
 
 pygame.quit()
