4de411d942ead3f58c8785b6c612134cfb26b5a6
Mohamed Abdelrahman <22403818@bucks.ac.uk>
2024-12-03 10:41:32 +0000

    Delete screen.py

--- screen.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-def game_screen(SCREEN_WIDTH, SCCREEN_HEIGHT):
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCCREEN_HEIGHT))
-    pygame.display.set_caption("Flappy Bird")
-    return screen
