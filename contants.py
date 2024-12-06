cbe0ea0f1dc7a8a559a248df0a1783fed2fddda8
Mohamed Abdelrahman <22403818@bucks.ac.uk>
2024-12-06 12:03:07 +0000

    Update contants.py

--- contants.py
+++ contants.py
@@ -1,13 +1,12 @@
 import pygame
 
-BIRD_WIDTH = 40
-BIRD_HEIGHT = 40
-BIRD_POSITION_X = 50
-BIRD_POSITION_Y = 50
-WINDOW_WIDTH = 1500
-WINDOW_HEIGHT = 750
-BG_COLOR = (250, 250, 250)  
+BIRD_WIDTH = 40 # bird width
+BIRD_HEIGHT = 40 # bird height
+BIRD_POSITION_X = 50 # X-co of the bird's starting position
+BIRD_POSITION_Y = 50 # Y-co of the bird's starting position
+WINDOW_WIDTH = 1500 # Game window width
+WINDOW_HEIGHT = 750 # Game window height
+BG_COLOR = (250, 250, 250) # Background color
 GAP_BETWEEN_PIPES = 220 # Gap between the top and bottom pipes
-GRAVITY = 0.5
-BIRD_COLOR = (250, 0, 0)
-
+GRAVITY = 0.5 # Force pulling the bird down
+BIRD_COLOR = (250, 0, 0) # Bird's color (Red)
