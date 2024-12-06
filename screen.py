3dcdb3e489fc5ad72ea3b9aa9a9c80efdcf92483
Mohamed Abdelrahman <22403818@bucks.ac.uk>
2024-12-06 11:58:56 +0000

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,7 +1,8 @@
 import pygame
 
-def game_screen(SCREEN_WIDTH, SCCREEN_HEIGHT):
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCCREEN_HEIGHT))
-    pygame.display.set_caption("Flappy Bird")
-    return screen
+def game_screen(SCREEN_WIDTH, SCCREEN_HEIGHT): # Seting up the game screen with the specified width and height
+    pygame.init() # Import the Pygame lib
+    # Display window with defined width and height
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCCREEN_HEIGHT)) # The dimensions are passed in a tuple to the set_mode function
+    pygame.display.set_caption("Flappy Bird") # Title of the window is set to "Flappy Bird"
+    return screen # The created screen object will be used further on in the game
